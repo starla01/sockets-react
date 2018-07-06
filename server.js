645fd0f8a943bdee45b51d443e98ee1d1a46357c
“Fernando <starla.frr@gmail.com>
2018-07-06 09:10:14 -0500

    Facebook Chat

--- server.js
+++ server.js
@@ -3,14 +3,9 @@ const http = require('http')
 const socketIO = require('socket.io')
 
 // our localhost port
-const port = 4001
-
-const app = express()
-
-console.log(app)
-
+const port = 4001;
+const app = express();
 app.use(function(req, res, next) {
-    console.log(res)
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -26,16 +21,18 @@ const io = socketIO(server)
 // This is what the socket.io syntax is like, we will work this later
 io.on('connection', socket => {
     console.log('New client connected')
-
-    // just like on the client side, we have a socket.on method that takes a callback function
-    socket.on('change color', (color) => {
-        // once we get a 'change color' event from one of our clients, we will send it to the rest of the clients
-        // we make use of the socket.emit method again with the argument given to use from the callback function above
-        console.log('Color Changed to: ', color)
-        io.sockets.emit('change color', color)
-    })
-
-    // disconnect is fired when a client leaves the server
+        // just like on the client side, we have a socket.on method that takes a callback function
+        // socket.on('change color', (color) => {
+        //     // once we get a 'change color' event from one of our clients, we will send it to the rest of the clients
+        //     // we make use of the socket.emit method again with the argument given to use from the callback function above
+        //     console.log('Color Changed to: ', color)
+        //     io.sockets.emit('change color', color)
+        // })
+    socket.on('chat', (msg) => {
+            console.log('Mensaje registrado', msg)
+            io.sockets.emit('chat', msg)
+        })
+        // disconnect is fired when a client leaves the server
     socket.on('disconnect', () => {
         console.log('user disconnected')
     })
--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
-      endpoint: "http://{IP server of ENDPOINT}:4001",
-      //endpoint: "http://192.168.11.132:4001",
-      color: 'white'
+      //endpoint: "http://{IP server of ENDPOINT}:4001",
+      endpoint: "http://192.168.14.160:4001",
+      color: 'white',
+      person: '',
+      message: '',
+      conversation: []
     }
   }
 
@@ -20,17 +23,74 @@ class App extends Component {
     this.setState({color})
   }
 
-  render() {
-    const socket = socketIOClient(this.state.endpoint)
-    socket.on('change color', (color) => {
-      document.body.style.backgroundColor = color
-    })
+  setName  = () => {
+    var person = prompt("Introduce tu nombre para la sala de Chat", "");
+    if (person != null) {
+      this.setState({person})
+    }
+  }
+  cleanText = () => {
 
+  }
+  handleMessage = (e) =>{
+    var _this = this;
+    var message = e.target.value;
+    
+
+    if(e.keyCode === 13){
+      e.target.value = '';
+      const socket = socketIOClient(this.state.endpoint)
+      socket.emit('chat', message)
+
+    }
+  }
+  componentDidMount(){
+    this.setName();
+
+    var messages = this.state.conversation;
+    const socket = socketIOClient(this.state.endpoint)
+    socket.on('chat', (msg) => {
+      messages.push(msg);
+      this.setState({conversation: messages})
+    });
+
+  }
+  render() {
+    
+    let datos = this.state.conversation.length ? this.state.conversation.map((data, key) => { 
+      var long = data.length
+      return <div className="envolveMsg"><span id={key} style={{textAlign: 'center', float: long > 40 ? 'right' : 'none'}} className="message" key={key}>{data}</span></div>
+    }) : null;
+
+    console.log(datos)
     return (
       <div style={{ textAlign: "center"}}>
-        <button onClick={() => this.send()}>Enviar</button>
+        {/* <button onClick={() => this.send()}>Enviar</button>
         <button id="blue" onClick={() => this.setColor('blue')}>Azul</button>
-        <button id="green" onClick={() => this.setColor('green')}>verde</button>
+        <button id="green" onClick={() => this.setColor('green')}>verde</button> */}
+        <div id="header"></div>
+        <div id="content">
+            <div id="colLeft"></div>
+            <div id="colCenter">
+                <div id="chat">
+                  <div className="chatTitle">
+                    <span className="name">{this.state.person}</span>
+                  </div>
+                  <div className="conversation">
+                    {datos}
+                  </div>
+                  <div className="messageContainer">
+                    <input type="text"
+                      className="message" 
+                      placeholder="Escribe un mensaje..."
+                      onKeyUp={ (e) => this.handleMessage(e)}
+                     />
+                  </div>
+                </div>
+            </div>
+            <div id="colRight"></div>
+        </div>
+        <div id="footer"></div>
       </div>
     );
   }
